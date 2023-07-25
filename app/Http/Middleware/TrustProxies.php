7299489e242b70f377d6023ccd99aac0b030998e
Tan Bunnchhay <bunchay1280@gmail.com>
2023-07-25 21:10:47 +0700

    update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -11,19 +11,18 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array<int, string>|string|null
+     * @var array
      */
-    protected $proxies;
-
+    protected $proxies = '*';
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
     protected $headers =
-        RequestAlias::HEADER_X_FORWARDED_FOR |
-        RequestAlias::HEADER_X_FORWARDED_HOST |
-        RequestAlias::HEADER_X_FORWARDED_PORT |
-        RequestAlias::HEADER_X_FORWARDED_PROTO |
-        RequestAlias::HEADER_X_FORWARDED_AWS_ELB;
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
